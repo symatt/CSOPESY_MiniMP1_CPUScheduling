7c398588885818b7397275b58ed484669945ea88
GodGacha <80673907+GodGacha@users.noreply.github.com>
2022-05-14 00:47:05 +0800

    updated rr

--- rr.h
+++ rr.h
@@ -12,10 +12,11 @@ struct Proc {
     int waitTime;
 };
 
-int rrWaitingTime(int processes[], int n, int burstTimes[], int arrivalTimes[], int waitTimes[], int quant, struct Proc proc[], int index[]) {
+int rrWaitingTime(int processes[], int n, int burstTimes[], int arrivalTimes[], int waitTimes[], int quant, struct Proc proc[], int fin[]) {
     int fill = 0, i, j, k;
-    int count = 0;
+    int count = 0, fincount = 0;
     int clear = 0;
+    int index[n];
 
     for (i=0; i<n; i++) {
         proc[i].process = 0;
@@ -28,47 +29,54 @@ int rrWaitingTime(int processes[], int n, int burstTimes[], int arrivalTimes[], 
 
     while (clear != 1) {
         clear = 0;
-        for (i=0; i<=fill; i++) {
-            for (j=0; j<n; j++) {
-                if (arrivalTimes[j] <= count) {
-                    for (k=0; k<fill; k++) {
-                        if (index[fill] != count) {
-                            index[fill] = j;
-                            fill++;
-                        }
+        for (i=0; i<n; i++) {
+            if (arrivalTimes[i] <= count) {
+                for (j=0; j<fill; j++) {
+                    if (index[fill] != count) {
+                        index[fill] = i;
+                        fill++;
                     }
                 }
             }
-
-            for (j=0; j<fill; j++) {
-                if (proc[index[j]].burstTime >= quant) {
-                    proc[index[j]].startTime[proc[index[j]].counter] = count;
-                    proc[index[j]].burstTime -= quant;
-                    count += quant;
-                    proc[index[j]].endTime[proc[index[j]].counter] = count;
-                    proc[index[j]].counter++;
-                    for (k=0; k<fill; k++) {
-                        if (proc[index[k]].process != proc[index[j]].process)
-                            proc[index[k]].waitTime += quant;
-                    }
-                }
-                else if (proc[index[j]].burstTime < quant && proc[index[j]].burstTime > 0) {
-                    proc[index[j]].startTime[proc[index[j]].counter] = count;
-                    count += proc[index[j]].burstTime;
-                    proc[index[j]].burstTime -= proc[index[j]].burstTime;
-                    proc[index[j]].endTime[proc[index[j]].counter] = count;
-                    proc[index[j]].counter++;
-                    for (k=0; k<fill; k++) {
-                        if (proc[index[k]].process != proc[index[j]].process)
-                            proc[index[k]].waitTime += proc[index[j]].burstTime;
-                    }
-                }
-            }
-
         }
-        // printf("NICE\n");
+
+            
         for (i=0; i<fill; i++) {
-            if (proc[index[i]].burstTime != 0) {
+            if (proc[index[i]].burstTime >= quant) {
+                proc[index[i]].startTime[proc[index[i]].counter] = count;
+                proc[index[i]].burstTime -= quant;
+                count += quant;
+                proc[index[i]].endTime[proc[index[i]].counter] = count;
+                proc[index[i]].counter++;
+                for (j=0; j<fill; j++) {
+                    if (proc[index[j]].process != proc[index[i]].process)
+                        proc[index[j]].waitTime += quant;
+                }
+                if (proc[index[i]].burstTime == 0) {
+                    fin[fincount] = index[i];
+                    fincount++;
+                }
+            }
+            else if (proc[index[i]].burstTime < quant && proc[index[j]].burstTime > 0) {
+                proc[index[i]].startTime[proc[index[i]].counter] = count;
+                count += proc[index[i]].burstTime;
+                proc[index[i]].burstTime -= proc[index[i]].burstTime;
+                proc[index[i]].endTime[proc[index[i]].counter] = count;
+                proc[index[i]].counter++;
+                for (j=0; j<fill; j++) {
+                    if (proc[index[j]].process != proc[index[i]].process)
+                        proc[index[j]].waitTime += proc[index[i]].burstTime;
+                }
+                if (proc[index[i]].burstTime == 0) {
+                    fin[fincount] = index[i];
+                    fincount++;
+                }
+            }
+        }
+
+        printf("NICE\n");
+        for (i=0; i<n; i++) {
+            if (proc[i].burstTime != 0) {
                 clear = 0;
                 break;
             }
@@ -81,17 +89,17 @@ int rrWaitingTime(int processes[], int n, int burstTimes[], int arrivalTimes[], 
 
 int rrGetAvgTime(int processes[], int n, int arrivalTimes[], int burstTimes[], int quant) {
     struct Proc proc[n];
-    int index[n];
+    int fin[n];
     int waitTimes[n], total_wt = 0, total_tat = 0, i, j;
-    rrWaitingTime(processes, n, burstTimes, arrivalTimes, waitTimes, quant, proc, index);
+    rrWaitingTime(processes, n, burstTimes, arrivalTimes, waitTimes, quant, proc, fin);
     
     for (i=0; i<n; i++) {
-      total_wt = total_wt + proc[index[i]].waitTime;
-      printf("P[%d] ", proc[index[i]].process);
-      for (j=0; j<proc[index[i]].counter; j++) {
-          printf("Start Time: %d  End Time: %d  |  ", proc[index[i]].startTime[j], proc[index[i]].endTime[j]);
+      total_wt = total_wt + proc[fin[i]].waitTime;
+      printf("P[%d] ", proc[fin[i]].process);
+      for (j=0; j<proc[fin[i]].counter; j++) {
+          printf("Start Time: %d  End Time: %d  |  ", proc[fin[i]].startTime[j], proc[fin[i]].endTime[j]);
       }
-      printf("Wait Time: %d\n", proc[index[i]].waitTime);
+      printf("Wait Time: %d\n", proc[fin[i]].waitTime);
     }
     printf("Average waiting time = %.1f", (float)total_wt / (float)n);
     return 0;
